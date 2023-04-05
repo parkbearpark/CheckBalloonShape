c8d2f993ff5848b29628ae6e0866b3311c72564d
yuma <yumai6205@gmail.com>
2023-04-05 21:49:25 +0900

    フキダシを抜き出してから文字を削除する

--- detect.py
+++ detect.py
@@ -17,12 +17,15 @@ def main(args):
 
     # コマの画像ファイルがあるディレクトリ
     input_dir = f'{args.folder}/panels'
-    # フキダシを囲む矩形を描画した画像を保存するディレクトリ
-    output_dir = f'{args.folder}/balloons'
+    # 画像を保存するディレクトリ
+    balloon_detected_output_dir = f'{args.folder}/balloons'
+    text_removed_output_dir = f'{args.folder}/text_removed'
 
     # ディレクトリが存在しない場合は作成
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+    if not os.path.exists(balloon_detected_output_dir):
+        os.mkdir(balloon_detected_output_dir)
+    if not os.path.exists(text_removed_output_dir):
+        os.mkdir(text_removed_output_dir)
 
     print('Detecting balloons...')
 
@@ -30,18 +33,28 @@ def main(args):
     input_paths = [os.path.join(input_dir, filename)
                    for filename in os.listdir(input_dir)]
 
-    # フキダシを検出して矩形で囲み、画像を保存
+    # フキダシを検出して切り取り、画像を保存
     images, locations, new_paths = detectFromPaths(
         input_paths)
     for i in range(len(images)):
         image = images[i]
-        location = locations[i]
         _new_path = new_paths[i].replace('.jpg', '')
         new_path = f'{_new_path}_{str(i)}.jpg'
         cv2.imwrite(os.path.join(
-            output_dir, os.path.basename(new_path)), image)
-
+            balloon_detected_output_dir, os.path.basename(new_path)), image)
     print('Detecting balloons... Done!')
+
+    print('Removing text...')
+
+    text_removed_imgs = panel_extractor.remove_text(images)
+    for i in range(len(text_removed_imgs)):
+        image = images[i]
+        _new_path = new_paths[i].replace('.jpg', '')
+        new_path = f'{_new_path}_{str(i)}.jpg'
+        cv2.imwrite(os.path.join(
+            text_removed_output_dir, os.path.basename(new_path)), image)
+
+    print('Removing text... Done!')
 
 
 if __name__ == '__main__':
